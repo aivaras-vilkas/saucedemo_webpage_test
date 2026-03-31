3db95ad4be7662e6f8db3375b6c1ccad4494a29f
aivaras-vilkas <vilkas.aivaras@gmail.com>
2026-03-31 21:25:13 +0300

    Initial Commit

---
+++ pages/LoginPage.ts
@@ -0,0 +1,23 @@
+import { Page } from "@playwright/test";
+
+export class LoginPage {
+  private usernameInput;
+  private passwordInput;
+  private loginButton;
+
+  constructor(private page: Page) {
+    this.usernameInput = page.locator('#user-name');
+    this.passwordInput = page.locator('#password');
+    this.loginButton = page.locator('#login-button');
+  }
+
+  async goToHomepage() {
+    await this.page.goto('/');
+  }
+
+  async login(username: string, password: string) {
+    await this.usernameInput.fill(username);
+    await this.passwordInput.fill(password);
+    await this.loginButton.click();
+  }
+}
